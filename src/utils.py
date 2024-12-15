19e518a12f2cdac3b32f0901a6a748275eabac13
LALAYANG <yangc9@illinois.edu>
2024-12-15 17:05:46 -0600

    add utils functions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 
 def read_jsonl(jsonl_path):
     jsonl_data = []
@@ -7,3 +8,21 @@ def read_jsonl(jsonl_path):
             json_obj = json.loads(line)
             jsonl_data.append(json_obj)
     return jsonl_data
+
+def write_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+        
+def run_python_code(filename):
+    try:
+        result = subprocess.run(['python3', filename], text=True, capture_output=True, check=True)
+        return result.stdout 
+    except subprocess.CalledProcessError as e:
+        return e.stdout + '\n' + e.stderr  # Return both stdout and stderr if an error occurs
+
+def evaluate_z3_code(tag, code):
+    tmp_file = f".tmp/{tag}_tmp.py"
+    write_file(tmp_file, code)
+    result = run_python_code(tmp_file)
+    return result
+    
