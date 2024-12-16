c0b3f3fd243aaf43041a7310f3c5ec79a385f034
LALAYANG <yangc9@illinois.edu>
2024-12-16 14:40:33 -0600

    add two func to run isabelle

--- src/utils.py
+++ src/utils.py
@@ -13,6 +13,30 @@ def write_file(file_path, content):
     with open(file_path, 'w') as file:
         file.write(content)
         
+def prep_isabelle_proof(code, name):
+    proof_folder_path = os.path.join("proof", name)
+    os.makedirs(proof_folder_path, exist_ok=True)
+
+    thy_path = os.path.join(proof_folder_path, f"{name}.thy")
+    with open(thy_path, 'w') as file:
+        file.write(code)
+    
+    root_path = os.path.join(proof_folder_path, "ROOT")
+    root_content = textwrap.dedent(f"""
+    session Test = HOL + 
+    theories
+        {name}
+    """)
+    with open(root_path, "w") as file:
+        file.write(root_content)
+        
+def run_isabelle_code(filename, folder_path):
+    try:
+        result = subprocess.run(['../../Isabelle2024/bin/isabelle', filename], cwd=folder_path, text=True, capture_output=True, check=True)
+        return result.stdout 
+    except subprocess.CalledProcessError as e:
+        return e.stdout + '\n' + e.stderr
+        
 def run_python_code(filename):
     try:
         result = subprocess.run(['python3', filename], text=True, capture_output=True, check=True)
