a26dbcde000994b84b635101780cbc51121d1248
LALAYANG <yangc9@illinois.edu>
2024-12-16 17:33:14 -0600

    fix for postprocessing new response

--- src/post_processing.py
+++ src/post_processing.py
@@ -8,7 +8,10 @@ def process_response(model, response):
     return rets 
 
 def parse_response_gpt(response):
-    instantiations = response.split("```instantiations")[-1].split("```")[0]
-    formal_proof = response.split("```formal_proof")[-1].split("```")[0]
+    phi_prime = response.split("```phi_prime\n")[-1].split("```")[0]
+    phi = response.split("```phi\n")[-1].split("```")[0]
+    skelomization = response.split("```skelomization\n")[-1].split("```")[0]
+    instantiations = response.split("```instantiations\n")[-1].split("```")[0]
+    final_formula = response.split("```final_formula\n")[-1].split("```")[0]
     z3_code = response.split("```python")[-1].split("```")[0] 
-    return instantiations, formal_proof, z3_code
+    return phi, phi_prime, skelomization, instantiations, final_formula, z3_code
